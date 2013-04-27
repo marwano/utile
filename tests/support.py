555f6c918398ec79c85ed7cc2c59fa076e953780
Marwan Alsabbagh <marwan.alsabbagh@gmail.com>
2013-04-27 17:25:50 +0300

    BaseTestCase more os-independent

--- tests/support.py
+++ tests/support.py
@@ -1,9 +1,4 @@
 
 import os.path
-from utile import safe_import
 
 BASE_DIR = os.path.dirname(__file__)
-
-lxml = safe_import('lxml')
-Crypto = safe_import('Crypto')
-bunch = safe_import('bunch')
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python
 
 import sys
-import unittest
+from unittest import TestCase, skipUnless
 from subprocess import check_output, CalledProcessError
 from utile import safe_import, encrypt, decrypt, shell_quote, flatten
-from support import Crypto
 import re
+import os.path
+Crypto = safe_import('Crypto')
 
 BYTES_ALL = ''.join(map(chr, range(256)))
 BYTES_ALL_BUT_NULL = ''.join(map(chr, range(1, 256)))
 
 
-class BaseTestCase(unittest.TestCase):
-    @unittest.skipIf(not Crypto, 'pycrypto not installed')
+class BaseTestCase(TestCase):
+    @skipUnless(Crypto, 'pycrypto not installed')
     def test_crypto(self):
         pairs = [
             ('secret_key', 'some data'),
             (BYTES_ALL, BYTES_ALL),
             (u'secret_key', u'some data'),
         ]
-        for key, data1 in pairs:
-            data2 = decrypt(key, encrypt(key, data1))
-            self.assertEqual(data1, data2)
+        for key, expected in pairs:
+            actual = decrypt(key, encrypt(key, expected))
+            self.assertEqual(expected, actual)
 
+    @skipUnless(os.path.exists('/bin/echo'), '/bin/echo not found')
     def test_shell_quote(self):
-        for data1 in ['testing...', BYTES_ALL_BUT_NULL]:
-            data2 = check_output('echo -n %s' % shell_quote(data1), shell=True)
-            self.assertEqual(data1, data2)
+        for expected in ['testing...', BYTES_ALL_BUT_NULL]:
+            cmd = '/bin/echo -n %s' % shell_quote(expected)
+            actual = check_output(cmd, shell=True)
+            self.assertEqual(expected, actual)
 
     def test_flatten(self):
-        self.assertEqual(flatten([range(3), range(3, 6)]), range(6))
+        self.assertEqual(flatten([(0, 1), (2, 3)]), range(4))
